04f4a23a8a3df4d632b9edd2cb9330513bca859b
pouria7gh <pouria.ghafarzadeh@gmail.com>
2026-07-06 16:30:11 +0330

    loop

---
+++ Chapter1/ElementsOfPython.py
@@ -0,0 +1,16 @@
+
+# x = 5
+# if (x < 10):
+#     print("Smaller than 10")
+# if (x > 10):
+#     print("Bigger than 10")
+# print("Finis")
+
+c = 20
+while (c > 0):
+    if (c % 3 == 0):
+        print("Hop")
+    else:
+        print(c)
+    c = c - 1
+print ("Blastoff!")
