e398827e8fdc9c86985f0b61c590925cce4217cd
pouria7gh <pouria.ghafarzadeh@gmail.com>
2026-07-08 22:40:10 +0330

    finish chapter 5.

---
+++ Chapter5/Loops.py
@@ -0,0 +1,34 @@
+### BREAK & CONTINUE ###
+
+# while True :
+#     line = input('> ')
+#     if line[0] == '#' : 
+#         continue
+#     if line.lower() == 'done' :
+#         break
+#     print(line)
+
+# print('Done!')
+
+### DEFINITE LOOPS ###
+
+# for i in [5, 4, 3, 2, 1] :
+#     print(i)
+# print('Blastoff')
+
+# friends = ['Ali', 'Iliya', 'Aria']
+# for friend in friends :
+#     print('Hello', friend)
+# print('done')
+
+# import math
+
+# nums = [9,8,7,6,5,4,3,2,1]
+# largest = -math.inf
+# for num in nums :
+#     if num > largest :
+#         largest = num
+# print(largest)
+
+for i in range(1, 4) :
+    print(i)
